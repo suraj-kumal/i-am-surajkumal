7b5ca6e2c82e12d9e988bfcf3171989f3354d664
Suraj Kumal <zerocoolofficial45@gmail.com>
2025-11-11 00:48:15 +0545

    remove site manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,7 +64,7 @@ export const metadata: Metadata = {
       { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
     ],
   },
-  manifest: "/site.webmanifest",
+  // manifest: "/site.webmanifest",
   openGraph: {
     type: "website",
     locale: "en_US",
